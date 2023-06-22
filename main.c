394a77a3d220e0ddb93a8d1a30145a3899b0cf5f
simone zacchetti <simone.zacchetti@mail.polimi.it>
2023-06-22 16:25:24 +0200

    inizio aggiunta lista ai nodi stazione

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_AUTO 512
 
+typedef struct Node {
+    int valore;
+    struct Node* next;
+} Node;
+
 typedef struct {
-    int numero_stazione;
+    int distanza;
     int auto_max;
     int autonomie_auto[MAX_AUTO];
+    Node* stazioni_adiacenti;
 } Stazione;
 
 typedef struct TreeNode {
@@ -15,6 +22,63 @@ typedef struct TreeNode {
     struct TreeNode* right;
 } TreeNode;
 
+//*** FUNZIONI PER GESTIONE LISTA ***//
+
+void inserisciInOrdineDecrescente(Node** testa, int valore) {
+    Node* nuovoNodo = (Node*)malloc(sizeof(Node));
+    nuovoNodo->valore = valore;
+    nuovoNodo->next = NULL;
+
+    if (*testa == NULL) {
+        // La lista è vuota, il nuovo Node diventa la testa
+        *testa = nuovoNodo;
+    } else {
+        if (valore >= (*testa)->valore) {
+            // Inserimento in testa
+            nuovoNodo->next = *testa;
+            *testa = nuovoNodo;
+        } else {
+            Node* current = *testa;
+            while (current->next != NULL && current->next->valore > valore) {
+                current = current->next;
+            }
+            // Inserimento dopo current
+            nuovoNodo->next = current->next;
+            current->next = nuovoNodo;
+        }
+    }
+}
+
+void cancella(Node** testa, int valore) {
+    if (*testa == NULL) {
+        // La lista è vuota
+        return;
+    }
+
+    Node* daCancellare = NULL;
+
+    if ((*testa)->valore == valore) {
+        // Il Node da cancellare è la testa
+        daCancellare = *testa;
+        *testa = (*testa)->next;
+    } else {
+        Node* current = *testa;
+        while (current->next != NULL && current->next->valore != valore) {
+            current = current->next;
+        }
+        if (current->next != NULL) {
+            daCancellare = current->next;
+            current->next = current->next->next;
+        }
+    }
+
+    if (daCancellare != NULL) {
+        free(daCancellare);
+    }
+}
+
+
+//*** FUNZIONI PER GESTIONE ALBERO ***//
 TreeNode* createNode(Stazione stazione) {
     TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
     newNode->stazione = stazione;
@@ -25,13 +89,17 @@ TreeNode* createNode(Stazione stazione) {
 
 TreeNode* insertNode(TreeNode* root, Stazione stazione) {
     if (root == NULL) {
+        printf("aggiunta\n");
         return createNode(stazione);
     }
 
-    if (stazione.numero_stazione < root->stazione.numero_stazione) {
+    if (stazione.distanza < root->stazione.distanza) {
         root->left = insertNode(root->left, stazione);
-    } else if (stazione.numero_stazione > root->stazione.numero_stazione) {
+    } else if (stazione.distanza > root->stazione.distanza) {
         root->right = insertNode(root->right, stazione);
+    } else {
+        // TODO verificare se è corretto il fatto che stampi se la stazione è già presente
+        printf("non aggiunta\n");
     }
 
     return root;
@@ -45,50 +113,59 @@ TreeNode* findMin(TreeNode* node) {
     return current;
 }
 
-TreeNode* deleteNode(TreeNode* root, int numero_stazione) {
+TreeNode* deleteNode(TreeNode* root, int distanza) {
     if (root == NULL) {
+        printf("non demolita\n");
         return root;
     }
 
-    if (numero_stazione < root->stazione.numero_stazione) {
-        root->left = deleteNode(root->left, numero_stazione);
-    } else if (numero_stazione > root->stazione.numero_stazione) {
-        root->right = deleteNode(root->right, numero_stazione);
+    if (distanza < root->stazione.distanza) {
+        root->left = deleteNode(root->left, distanza);
+    } else if (distanza > root->stazione.distanza) {
+        root->right = deleteNode(root->right, distanza);
     } else {
         if (root->left == NULL) {
             TreeNode* temp = root->right;
             free(root);
+            printf("demolita\n");
             return temp;
         } else if (root->right == NULL) {
             TreeNode* temp = root->left;
             free(root);
+            printf("demolita\n");
             return temp;
         }
 
         TreeNode* temp = findMin(root->right);
         root->stazione = temp->stazione;
-        root->right = deleteNode(root->right, temp->stazione.numero_stazione);
+        root->right = deleteNode(root->right, temp->stazione.distanza);
     }
 
     return root;
 }
 
-TreeNode* searchNode(TreeNode* root, int numero_stazione) {
-    if (root == NULL || root->stazione.numero_stazione == numero_stazione) {
+TreeNode* searchNode(TreeNode* root, int distanza) {
+    if (root == NULL || root->stazione.distanza == distanza) {
         return root;
     }
 
-    if (numero_stazione < root->stazione.numero_stazione) {
-        return searchNode(root->left, numero_stazione);
+    if (distanza < root->stazione.distanza) {
+        return searchNode(root->left, distanza);
     } else {
-        return searchNode(root->right, numero_stazione);
+        return searchNode(root->right, distanza);
     }
 }
 
 void inorderTraversal(TreeNode* root) {
     if (root != NULL) {
         inorderTraversal(root->left);
-        printf("Numero stazione: %d\n", root->stazione.numero_stazione);
+        printf("Numero stazione: %d\n", root->stazione.distanza);
+        for(int i=0; i<MAX_AUTO; i++) {
+            if (root->stazione.autonomie_auto[i] != -1) {
+                printf("Auto %d: %d\n", i, root->stazione.autonomie_auto[i]);
+            }
+        }
+        printf("max: %d\n", root->stazione.auto_max);
         inorderTraversal(root->right);
     }
 }
@@ -101,45 +178,189 @@ void freeTree(TreeNode* root) {
     }
 }
 
+//*** FUNZIONI PER GESTIONE COMANDI RICEVUTI DA INPUT ***//
+void aggiungiStazione(TreeNode** root, int distanza, int numero_auto, int autonomie[]) {
+    Stazione stazione;
+    stazione.distanza = distanza;
+    stazione.auto_max = 0;
+    for (int i = 0; i < MAX_AUTO; i++) {
+        if (i < numero_auto) {
+            stazione.autonomie_auto[i] = autonomie[i];
+            if (autonomie[i] > stazione.auto_max) {
+                stazione.auto_max = autonomie[i];
+            }
+        } else {
+            stazione.autonomie_auto[i] = -1;
+        }
+    }
+
+    *root = insertNode(*root, stazione);
+}
+
+void comandoAggiungiStazione(TreeNode** root, char* restoStringa) {
+    // Implementazione per l'aggiunta della stazione
+    // Utilizza il parametro restoStringa per ottenere gli argomenti necessari
+
+    // Esempio di implementazione:
+    int distanza;
+    int numero_auto;
+    int autonomie[MAX_AUTO];
+
+    char* token = strtok(restoStringa, " ");
+    if (token == NULL) {
+        printf("Errore: specificare la distanza della stazione.\n");
+        return;
+    }
+    distanza = atoi(token);
+
+    token = strtok(NULL, " ");
+    if (token == NULL) {
+        printf("Errore: specificare il numero di auto.\n");
+        return;
+    }
+    numero_auto = atoi(token);
+
+    int i = 0;
+    while (i < numero_auto) {
+        token = strtok(NULL, " ");
+        if (token == NULL) {
+            printf("Errore: specificare le autonomie delle auto.\n");
+            break;
+        }
+        autonomie[i] = atoi(token);
+        i++;
+    }
+
+    if (i == numero_auto) {
+        aggiungiStazione(root, distanza, numero_auto, autonomie);
+    }
+}
+
+void aggiungiAuto(TreeNode* stazione, int autonomia) {
+    Stazione* staz = &(stazione->stazione);
+
+    int i;
+    for (i = 0; i < MAX_AUTO; i++) {
+        if (staz->autonomie_auto[i] == -1) {
+            staz->autonomie_auto[i] = autonomia;
+            if (autonomia > staz->auto_max) {
+                staz->auto_max = autonomia;
+            }
+            printf("aggiunta\n");
+            break;
+        }
+    }
+
+    if (i == MAX_AUTO) {
+        printf("Errore: il numero massimo di auto per stazione è stato raggiunto.\n");
+    }
+}
+
+// TODO cosi viene tolta solo la prima auto trovata con tale autonomia
+void demolisciAuto(TreeNode* stazione, int autonomia) {
+    Stazione* staz = &(stazione->stazione);
+
+    int i;
+    for (i = 0; i < MAX_AUTO; i++) {
+        if (staz->autonomie_auto[i] == autonomia) {
+            staz->autonomie_auto[i] = -1;
+            if (autonomia == staz->auto_max) {
+                staz->auto_max = 0;
+                for (int j = 0; j < MAX_AUTO; j++) {
+                    if (staz->autonomie_auto[j] > staz->auto_max) {
+                        staz->auto_max = staz->autonomie_auto[j];
+                    }
+                }
+            }
+            printf("demolita\n");
+            break;
+        }
+    }
+
+    if (i == MAX_AUTO) {
+        printf("non rottamata\n");
+    }
+}
+
 int main() {
     TreeNode* root = NULL;
 
-    // Inserimento di alcune stazioni
-    Stazione stazione1 = {1, 10, {100, 200, 300}};
-    Stazione stazione3 = {3, 8, {120, 220, 320}};
-    Stazione stazione4 = {4, 12, {180, 280, 380}};
-    Stazione stazione2 = {2, 5, {150, 250, 350}};
+    char* comando = NULL;
+    size_t buffer_size = 0;
+    ssize_t input_length;
 
-    root = insertNode(root, stazione1);
-    root = insertNode(root, stazione2);
-    root = insertNode(root, stazione3);
-    root = insertNode(root, stazione4);
+    while (1) {
+        printf("Inserisci il comando: ");
+        input_length = getline(&comando, &buffer_size, stdin);
 
-    // Stampa dell'albero in ordine
-    printf("Albero in ordine:\n");
-    inorderTraversal(root);
+        if (input_length == -1) {
+            // Errore di input
+            printf("Errore durante la lettura dell'input.\n");
+            break;
+        }
 
-    // Esempio di ricerca di una stazione
-    int numero_stazione_da_cercare = 2;
-    TreeNode* result = searchNode(root, numero_stazione_da_cercare);
-    if (result != NULL) {
-        printf("\nStazione trovata con numero: %d\n", result->stazione.numero_stazione);
-        printf("Capacità massima auto: %d\n", result->stazione.auto_max);
-    } else {
-        printf("\nStazione non trovata\n");
+        // Rimuovo il carattere di newline alla fine del comando
+        comando[strcspn(comando, "\n")] = '\0';
+
+        char* token = strtok(comando, " ");
+        if (token != NULL) {
+            if (strcmp(token, "aggiungi-stazione") == 0) {
+                // salvo il resto della stringa in token, NULL: leggo dalla stessa stringa di prima, "": non ci sono altri separatori
+                token = strtok(NULL, "");
+                if (token != NULL) {
+                    comandoAggiungiStazione(&root, token);
+                }
+            } else if (strcmp(token, "demolisci-stazione") == 0) {
+                token = strtok(NULL, "");
+                if (token != NULL) {
+                    int distanza = atoi(token);
+                    root = deleteNode(root, distanza);
+                }
+            } else if (strcmp(token, "aggiungi-auto") == 0){
+                token = strtok(NULL, " ");
+                if (token != NULL) {
+                    int distanza = atoi(token);
+                    TreeNode* stazione = searchNode(root, distanza);
+                    if (stazione != NULL) {
+                        token = strtok(NULL, "");
+                        if (token != NULL) {
+                            int autonomia = atoi(token);
+                            aggiungiAuto(stazione, autonomia);
+                        }
+                    } else {
+                        printf("non aggiunta\n");
+                    }
+                }
+            } else if (strcmp(token, "rottama-auto") == 0){
+                token = strtok(NULL, " ");
+                if (token != NULL) {
+                    int distanza = atoi(token);
+                    TreeNode* stazione = searchNode(root, distanza);
+                    if (stazione != NULL) {
+                        token = strtok(NULL, "");
+                        if (token != NULL) {
+                            int autonomia = atoi(token);
+                            demolisciAuto(stazione, autonomia);
+                        }
+                    } else {
+                        printf("non rottamata\n");
+                    }
+                }
+            } else if (strcmp(token, "pianifica-percorso") == 0){
+
+            }
+            else {
+                printf("Comando non valido.\n");
+            }
+        }
+        inorderTraversal(root);
     }
-
-    // Esempio di cancellazione di una stazione
-    int numero_stazione_da_cancellare = 1;
-    root = deleteNode(root, numero_stazione_da_cancellare);
-
-    // Stampa dell'albero dopo la cancellazione
-    printf("\nAlbero dopo la cancellazione:\n");
-    inorderTraversal(root);
 
     // Liberazione della memoria occupata dall'albero
     freeTree(root);
-    //ciao
+
+    // Liberazione della memoria occupata dal buffer dell'input
+    free(comando);
 
     return 0;
 }
